24da7efa059f3f3a6080686914525809ca80125c
Clint Andrew Hall <clint@clintandrewhall.com>
2014-11-13 09:05:39 -0800

    Cl0wnt0wn

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ Metaphors.buildLibrary('/public/md', { 'cache': true }, function(err, library) {
           "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
           "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
           "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
-          "  debugger;ga('create', 'UA-55937322-1', 'auto');" +
+          "  ga('create', 'UA-55937322-1', 'auto');" +
           "  ga('send', 'pageview');" +
           "</script>" +
         '</body></html>'
