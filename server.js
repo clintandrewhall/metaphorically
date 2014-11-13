9780c6adadea471c4ee0f46b346353e828082af4
Clint Andrew Hall <clint@clintandrewhall.com>
2014-11-13 08:50:09 -0800

    Google Analytics

--- server.js
+++ server.js
@@ -63,7 +63,15 @@ Metaphors.buildLibrary('/public/md', { 'cache': true }, function(err, library) {
           '  <link rel="stylesheet" href="/public/css/skeleton.css" />\n' +
           '  <link rel="stylesheet" href="/public/css/layout.css" />\n' +
           '  <link rel="stylesheet" href="/public/css/style.css" />\n' +
-          '</head>' + markup + '</html>'
+          '</head><body>' + markup + '<script>' +
+          "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ " +
+          "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
+          "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
+          "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
+          "  debugger;ga('create', 'UA-55937322-1', 'auto');" +
+          "  ga('send', 'pageview');" +
+          "</script>" +
+        '</body></html>'
         );
       });
     })
